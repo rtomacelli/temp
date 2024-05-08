905ba557f3a68932affb0d2b210c23192f74fe5f
Ricardo Tomacelli <66192950+rtomacelli@users.noreply.github.com>
2024-05-08 12:56:42 -0300

    Update lista_paginada.js

--- lista_paginada.js
+++ lista_paginada.js
@@ -93,13 +93,9 @@ export default App;
 
 
 
-
-
-import React, { useState } from 'react';
-
+import React from 'react';
 import Layout from "@components/Layout";
-import PaginatedList from '@components/interacao/ListaInteracoes';
-
+import ListaInteracoes from '@components/interacao/ListaInteracoes';
 import {
   BbCard,
   BbCardHeader,
@@ -109,31 +105,16 @@ import {
 } from "dls-bb-web-components-react-wrapper";
 
 const Home: React.FC = (): JSX.Element => {
-
-
-
   return (
     <Layout>
       <BbCard>
-        <BbCardHeader>Marvin suporte React Blank</BbCardHeader>
+        <BbCardHeader>Exemplo de Utilização de ListaInteracoes Component</BbCardHeader>
         <BbCardBody>
-          Esse é um projeto blank que ajuda você a dar o ponta pé inicial em uma aplicação dentro do
-          APW, para mais informações acesse a documentação oficial do APW clicando{" "}
-          <a target="_blank" rel="noreferrer" href="https://apw.apps.bb.com.br/">
-            aqui
-          </a>
-          <div className="wrapper">
           <ListaInteracoes />
-          </div>
         </BbCardBody>
-        <BbCardFooter>
-          <BbButton>Confirmar</BbButton>
-          <BbButton kind="secondary">Cancelar</BbButton>
-        </BbCardFooter>
       </BbCard>
     </Layout>
   );
 };
 
 export default Home;
-
